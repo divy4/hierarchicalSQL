e72f3ce19c8fd5aef659b89240248e89b801d58b
Dan Ivy <danivy4@gmail.com>
2018-04-09 13:07:28 -0500

    Added operator testing

--- util.php
+++ util.php
@@ -14,10 +14,10 @@ function validBrackets($query, $brackets) {
     // for char in query
     for ($i = 0; $i < $qLen; $i++) {
         $bracket = $query[$i];
-        // open parenthesis
+        // open bracket
         if (array_key_exists($bracket, $brackets)) {
             $seenBrackets->push($bracket);
-        // close parenthesis
+        // close bracket
         } elseif (in_array($bracket, $brackets)) {
             // too many closings
             if ($seenBrackets->count() == 0) {
@@ -35,6 +35,44 @@ function validBrackets($query, $brackets) {
 }
 
 /**
+ * Tests if the operators in $query is valid. i.e. in every instance of )*(, where ( is an opening bracket and ) is a closing bracket, * is an operator.
+ *
+ * @param [String] $query The text of a query.
+ * @param [Array[String => String]] $brackets An associative array that maps an openning bracket to it's closing bracket.
+ * @param [Array[String => String]] $operators An associative Array that maps an operator's string (e.g. 'OR') to a string that represents how the "score" column should be calculated in SQL when preforming a natural join between tables t1 and t2 that both contain a "score" column.
+ * @return void
+ */
+function validOperators($query, $brackets, $operators) {
+    $lastOpIsClose = false;
+    $operator = "";
+    $qLen = strlen($query);
+    // for char in query
+    for ($i = 0; $i < $qLen; $i++) {
+        $char = $query[$i];
+        // open bracket
+        if (array_key_exists($char, $brackets)) {
+            if ($lastOpIsClose) {
+                // remove white space
+                $operator = \trim($operator);
+                // invalid operator
+                if (!array_key_exists($operator, $operators)) {
+                    return false;
+                }
+            }
+            $lastOpIsClose = false;
+        // close bracket
+        } elseif (in_array($char, $brackets)) {
+            $lastOpIsClose = true;
+            $operator = "";
+        // not bracket
+        } else {
+            $operator = $operator . $char;
+        }
+    }
+    return true;
+}
+
+/**
  * Calculates the depth of a query.
  *
  * @param [String] $query The text of a query.
@@ -48,15 +86,16 @@ function depth($query, $brackets) {
     // for char in query
     for ($i = 0; $i < $qLen; $i++) {
         $bracket = $query[$i];
-        // open parenthesis
+        // open bracket
         if (array_key_exists($bracket, $brackets)) {
             $depth++;
             $max = max($max, $depth);
-        // close parenthesis
+        // close bracket
         } elseif (in_array($bracket, $brackets)) {
             $depth--;
         }
     }
+    return $max;
 }
 
 
@@ -69,13 +108,14 @@ function depth($query, $brackets) {
  * @param [Integer] $maxDepth The maximum depth of brackets the query can contain.
  * @param [Integer] $maxSubQueries The maximum number of subqueries (i.e. the number of $baseParser calls) the query can contain.
  */
-function valideQuery($query, $brackets, $operators, $maxDepth, $maxSubqueries) {
+function validQuery($query, $brackets, $operators, $maxDepth, $maxSubqueries) {
     if (!validBrackets($query, $brackets)) {
         return false;
     }
     if (depth($query, $brackets) != $maxDepth) {
         return false;
     }
+    
     return true;
 }
 
